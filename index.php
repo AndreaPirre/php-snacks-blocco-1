de07bc1253039b45e23e671f1beee0166dbf0260
Andrea <andreapirrello1990@gmail.com>
2020-09-02 17:08:10 +0200

    snack 4

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+// snack 4
+
+function getArrayOfIntegers($min, $max, $nItems)
+{
+    $min = '1';
+    $max = '100';
+    $nItems = '15';
+    $newArray = [];
+
+
+        while (count($newArray) < $nItems) {
+        $number = rand($min, $max);
+        if (!in_array($number, $newArray)) {
+        $newArray[] = $number;
+        }
+    }
+    return $newArray;
+}
+
+?>
