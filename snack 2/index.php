a6dbc144bdd5a4bde92f1257d129b03b2d0433aa
Andrea <andreapirrello1990@gmail.com>
2020-09-02 18:45:42 +0200

    aggiunta condizione name

--- snack 2/index.php
+++ snack 2/index.php
@@ -13,6 +13,17 @@
     $dotPresent = strpos($mail, '.');
     $atPresent = strpos($mail, '@');
 
+    $lenghtName = strlen($name);
+
+    if ($lenghtName > 3) {
+
+        echo 'nome valido';
+
+    } else {
+
+        echo 'nome non valido';
+    }
+
     if ($dotPresent !== false && $atPresent !== false) {
 
         echo "ok";
